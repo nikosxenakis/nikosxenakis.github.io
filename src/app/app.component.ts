368e36c226d06c95a2faa94a0de906dfefe6c35a
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2018-10-21 20:28:23 +0100

    website updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
 	linkedinPage = 'https://www.linkedin.com/in/nikos-xenakis-875811109/';
 	instagramPage = 'https://www.instagram.com/nikosxenakis/';
 
-	CVLink = 'https://github.com/nikosxenakis/cv/blob/master/Xenakis_Nikos_CV.pdf';
+	CVLink = 'https://github.com/nikosxenakis/resume/blob/master/Xenakis_Nikos_resume.pdf';
 	imageInLexis = 'assets/ImagInLexis/ImagInLexis.zip';
 	iceEditor = 'https://github.com/nikosxenakis/ICE_Editor';
 }
